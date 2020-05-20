8ae077d5efffa6980f365cb7a4f50ffcd1a4f6de
SliceCast <63622684+SliceCast@users.noreply.github.com>
2020-05-20 15:54:06 -0400

    Removed libName on offsets because of new updated mod menu template.

--- Source-Code/main.cpp
+++ Source-Code/main.cpp
@@ -90,38 +90,38 @@ void octo_hook(void *orig_fcn, void* new_fcn, void **orig_fcn_ptr)
 #endif
 }
 
-void *(*Component_GetTransform)(void* component) = (void *(*)(void* ))getAbsoluteAddress(libName, 0xE6EBD8); // Component$$get_transform
-void *(*Component_GetGameObject)(void* component) = (void *(*)(void* ))getAbsoluteAddress(libName, 0xE6EC68); // Component$$get_gameObject
+void *(*Component_GetTransform)(void* component) = (void *(*)(void* ))getAbsoluteAddress(0xE6EBD8); // Component$$get_transform
+void *(*Component_GetGameObject)(void* component) = (void *(*)(void* ))getAbsoluteAddress(0xE6EC68); // Component$$get_gameObject
 
-Vector3 (*Transform_get_position)(void* transform) = (Vector3 (*)(void *))getAbsoluteAddress(libName, 0x38E6234); // Transform$$get_position
-void (*Transform_Set_Position)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(libName, 0x3A87434); // Transform$$set_position
-void (*Transform_Get_parent)(void* transform) = (void (*)(void*))getAbsoluteAddress(libName, 0x3A884D4); // Transform$$get_parent
+Vector3 (*Transform_get_position)(void* transform) = (Vector3 (*)(void *))getAbsoluteAddress(0x38E6234); // Transform$$get_position
+void (*Transform_Set_Position)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(0x3A87434); // Transform$$set_position
+void (*Transform_Get_parent)(void* transform) = (void (*)(void*))getAbsoluteAddress(0x3A884D4); // Transform$$get_parent
 //void (*Transform_Set_Rotation)(void* transform, Quaternion pos) = (void (*)(void*, Quaternion))getRealOffset(0x2159EC0);
 //Quaternion (*Transform_Get_Rotation)(void* transform) = (Quaternion (*)(void*))getRealOffset(0x2159E30);
-void (*Transform_LookAt)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(libName, 0x3A89368); // public void LookAt(Vector3 worldPosition)
-void (*Transform_Rotate)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(libName, 0x3A88DC8); // public void Rotate(Vector3 eulers)
-void (*Transform_set_localScale)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(libName, 0x3A88434); // Transform$$set_localScale
-Vector3 (*Transform_get_localScale)(void* transform) = (Vector3 (*)(void *))getAbsoluteAddress(libName, 0x3A8836C); // Transform$$get_localScale
+void (*Transform_LookAt)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(0x3A89368); // public void LookAt(Vector3 worldPosition)
+void (*Transform_Rotate)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(0x3A88DC8); // public void Rotate(Vector3 eulers)
+void (*Transform_set_localScale)(void* transform, Vector3 pos) = (void (*)(void*, Vector3))getAbsoluteAddress(0x3A88434); // Transform$$set_localScale
+Vector3 (*Transform_get_localScale)(void* transform) = (Vector3 (*)(void *))getAbsoluteAddress(0x3A8836C); // Transform$$get_localScale
 
-void (*GameObject_set_active)(void* obj, bool) = (void (*)(void* obj, bool))getAbsoluteAddress(libName, 0x39241F4); // GameObject$$set_active
+void (*GameObject_set_active)(void* obj, bool) = (void (*)(void* obj, bool))getAbsoluteAddress(0x39241F4); // GameObject$$set_active
 //void *(*PhotonNetwork_InstantiateSceneObject)(void* _this,void* prefabName, Vector3 position, Quaternion rotation, int group, void*) = (void *(*)(void*, void*, Vector3, Quaternion, int, void*))getRealOffset(0x947E7C); // PhotonNetwork$$InstantiateSceneObject
 
 bool isEnemy(void* me, void* it)
 {
     bool (*isEnemy)(void *me, void *it) =
-    (bool(*)(void *, void *))getAbsoluteAddress(libName, 0x18B44BC); // PlayerDamageable$$IsEnemyTo
+    (bool(*)(void *, void *))getAbsoluteAddress(0x18B44BC); // PlayerDamageable$$IsEnemyTo
     return isEnemy(me, it);
 }
 bool isDead(void* it)
 {
     bool (*IsDead)(void *me) =
-    (bool(*)(void *))getAbsoluteAddress(libName, 0x18B47E4); // PlayerDamageable$$IsDead
+    (bool(*)(void *))getAbsoluteAddress(0x18B47E4); // PlayerDamageable$$IsDead
     return IsDead(it);
 }
 
 void* get_weaponManagerStatic()
 {
-    void *var = *(void **) getAbsoluteAddress(libName, 0x4D1D724); // Class$WeaponManager
+    void *var = *(void **) getAbsoluteAddress(0x4D1D724); // Class$WeaponManager
     if (var)
     {
         void* var1 = *(void **)((uint64_t)var + 0x5C);
@@ -349,20 +349,20 @@ void* hack_thread(void*) {
     } while (!isLibraryLoaded(libName));
     LOGI("I found the il2cpp lib. Address is: %p", (void*)findLibrary(libName));
     LOGI("Hooking GameManager_LateUpdate");
-    AddGems = (void (*)(void*, int, bool))getAbsoluteAddress(libName, 0x211B030);
-    octo_hook((void*)getAbsoluteAddress(libName, 0x1248414), (void*)PlayerMoveCUpdate, (void**)&old_PlayerMoveCUpdate);
-    octo_hook((void*)getAbsoluteAddress(libName, 0x16025CC), (void*)WeapSoundsUpdate, (void**)&old_WeapSoundsUpdate);
-    octo_hook((void*)getAbsoluteAddress(libName, 0xE6A66C), (void*)get_fieldOfView, (void**)&old_get_fieldOfView);
-    octo_hook((void*)getAbsoluteAddress(libName, 0x17BC674), (void*)getlevel, (void**)&old_getlevel);
-    octo_hook((void*)getAbsoluteAddress(libName, 0x21169A8), (void*)BankController, (void**)&old_BankController);
-    patch.HealthRegeneration = Patch::Setup((void*)getAbsoluteAddress(libName, 0x22F0A3C), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
-    patch.RecoilDisabled = Patch::Setup((void*)getAbsoluteAddress(libName, 0x22F6C1C), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
-    patch.GrenadeReload = Patch::Setup((void*)getAbsoluteAddress(libName, 0x22F6CF0), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
-    patch.RecoverDamage = Patch::Setup((void*)getAbsoluteAddress(libName, 0x22F6DC4), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
-    patch.WeaponDamage = Patch::Setup((void*)getAbsoluteAddress(libName, 0x126388C), (char*)"\xFA\x04\x44\xE3\x1E\xFF\x2F\xE1", 8);
-    patch.currentammoinclip = Patch::Setup((void*)getAbsoluteAddress(libName, 0x1FD57CC), (char*)"\xDC\x0F\x0F\xE3\x1e\xff\x2f\xe1", 8);
-    patch.rapidfire = Patch::Setup((void*)getAbsoluteAddress(libName, 0x14C592C), (char*)"\xFA\x04\x44\xE3\x1E\xFF\x2F\xE1", 8);
-    patch.BulletSpread = Patch::Setup((void*)getAbsoluteAddress(libName, 0x1287BEC), (char*)"\x00\x00\xA0\xE3\x1E\xFF\x2F\xE1", 8);
+    AddGems = (void (*)(void*, int, bool))getAbsoluteAddress(0x211B030);
+    octo_hook((void*)getAbsoluteAddress(0x1248414), (void*)PlayerMoveCUpdate, (void**)&old_PlayerMoveCUpdate);
+    octo_hook((void*)getAbsoluteAddress(0x16025CC), (void*)WeapSoundsUpdate, (void**)&old_WeapSoundsUpdate);
+    octo_hook((void*)getAbsoluteAddress(0xE6A66C), (void*)get_fieldOfView, (void**)&old_get_fieldOfView);
+    octo_hook((void*)getAbsoluteAddress(0x17BC674), (void*)getlevel, (void**)&old_getlevel);
+    octo_hook((void*)getAbsoluteAddress(0x21169A8), (void*)BankController, (void**)&old_BankController);
+    patch.HealthRegeneration = Patch::Setup((void*)getAbsoluteAddress(0x22F0A3C), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
+    patch.RecoilDisabled = Patch::Setup((void*)getAbsoluteAddress(0x22F6C1C), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
+    patch.GrenadeReload = Patch::Setup((void*)getAbsoluteAddress(0x22F6CF0), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
+    patch.RecoverDamage = Patch::Setup((void*)getAbsoluteAddress(0x22F6DC4), (char*)"\x01\x00\xa0\xe3\x1e\xff\x2f\xe1", 8);
+    patch.WeaponDamage = Patch::Setup((void*)getAbsoluteAddress(0x126388C), (char*)"\xFA\x04\x44\xE3\x1E\xFF\x2F\xE1", 8);
+    patch.currentammoinclip = Patch::Setup((void*)getAbsoluteAddress(0x1FD57CC), (char*)"\xDC\x0F\x0F\xE3\x1e\xff\x2f\xe1", 8);
+    patch.rapidfire = Patch::Setup((void*)getAbsoluteAddress(0x14C592C), (char*)"\xFA\x04\x44\xE3\x1E\xFF\x2F\xE1", 8);
+    patch.BulletSpread = Patch::Setup((void*)getAbsoluteAddress(0x1287BEC), (char*)"\x00\x00\xA0\xE3\x1E\xFF\x2F\xE1", 8);
     return NULL;
 }
 
