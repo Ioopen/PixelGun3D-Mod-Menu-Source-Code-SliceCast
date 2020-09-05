47cc9f5efbff9e1a386317f797aef5c3adfebd55
SliceCast <63622684+SliceCast@users.noreply.github.com>
2020-09-05 07:41:51 -0400

    Offsets for main.cpp

---
+++ Source-Code/Includes/Offsets.h
@@ -0,0 +1,24 @@
+#ifndef OFFSETS_H
+#define OFFSETS_H
+
+namespace Offsets {
+    enum Offsets {
+        Rget_healthregen = 0x276A14C,
+        Rget_armorregen = 0x276A0C0,
+        Rget_ninjajump = 0x2766C6C,
+        Rget_JumpModifier = 0x2765FD8,
+        Rget_ScatterReduction = 0x276FB3C,
+        Rget_AlwaysCriticalHit = 0x276FD8C,
+        Rget_DisabledRecoil = 0x277032C,
+        RGadgetCooldown = 0x276F608,
+        RSpeedModifier = 0x276B530,
+        RSendChatOffset = 0x18428B4,
+        RPlayer_move_c_Update = 0x186A240,
+        RWeaponSoundsUpdateOffset = 0x15DD314,
+        RBankControllerUpdate = 0x25FE5E8,
+        RFirstPersonControlSharpUpdate = 0x21AD640,
+        Rget_CurrentLevel = 0x1DBE48C
+    };
+}
+
+#endif
